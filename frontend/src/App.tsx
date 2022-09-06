3c1d06297a51d3c751f2d980cfd965a9de0995c5
eniojb <eniojb@gmail.com>
2022-09-06 11:10:33 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
